be4d9dcb99f603045b7e42d4c7824288dbfab6ac
Ulises Ubuntu <ulises.entheos@gmail.com>
2024-12-10 14:44:03 +0100

    v0.2wip

--- backtracking.c
+++ backtracking.c
@@ -12,21 +12,31 @@
 
 #include "longlib.h"
 
-int	path_find(char **matrix, int y, int x)
+int	path_find(char **matrix, int y, int x, int flag)
 {
-	matrix[y][x] = 'P';
-	ft_printf("Player: %i-%i\n", y, x);
 	if (matrix[y - 1][x] == 'E' || matrix[y][x - 1] == 'E' ||
 		matrix[y][x + 1] == 'E' || matrix[y + 1][x] == 'E')
 		return (0);
-	if (matrix[y - 1][x] == '0' || matrix[y - 1][x] == 'C')
-		path_find(matrix, y - 1, x);
-	if (matrix[y][x - 1] == '0' || matrix[y][x - 1] == 'C')
-		path_find(matrix, y, x - 1);
-	if (matrix[y][x + 1] == '0' || matrix[y][x + 1] == 'C')
-		path_find(matrix, y, x + 1);
-	if (matrix[y + 1][x] == '0' || matrix[y + 1][x] == 'C')
-		path_find(matrix, y + 1, x);
+	if (flag != 1)
+	{
+		if (matrix[y - 1][x] == '0' || matrix[y - 1][x] == 'C')
+			path_find(matrix, y - 1, x, 1);
+	}
+	if (flag != 2)
+	{
+		if (matrix[y][x - 1] == '0' || matrix[y][x - 1] == 'C')
+			path_find(matrix, y, x - 1, 2);
+	}
+	if (flag != 3)
+	{
+		if (matrix[y][x + 1] == '0' || matrix[y][x + 1] == 'C')
+			path_find(matrix, y, x + 1, 3);
+	}
+	if (flag != 4)
+	{
+		if (matrix[y + 1][x] == '0' || matrix[y + 1][x] == 'C')
+			path_find(matrix, y + 1, x, 4);
+	}
 	return (1);
 }
 
@@ -64,7 +74,7 @@ int	backtracking(char **matrix)
 	x = 0;
 	if (player_pos(matrix, &y, &x))
 		return (1);
-	if (path_find(matrix, y, x))
+	if (path_find(matrix, y, x, 0))
 		return (1);
 	return (0);
 }
