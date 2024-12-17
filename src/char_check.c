22ab50f8a48d53d1f132bb2ac76e80a49b846e91
Ulises Zeta <ulises.entheos@gmail.com>
2024-12-17 14:51:03 +0100

    v0.2wip

--- src/char_check.c
+++ src/char_check.c
@@ -6,11 +6,23 @@
 /*   By: ulfernan <ulfernan@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 10:54:05 by ulfernan          #+#    #+#             */
-/*   Updated: 2024/12/17 00:05:28 by ulfernan         ###   ########.fr       */
+/*   Updated: 2024/12/17 00:17:26 by ulfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "longlib.h"
+
+int	characters(char matrix)
+{
+	if ((matrix != '1')
+		&& (matrix != '0')
+		&& (matrix != 'P')
+		&& (matrix != 'C')
+		&& (matrix != 'E')
+		&& (matrix != '\n'))
+		return (1);
+	return (0);
+}
 
 void	collection(t_data_load *load)
 {
@@ -50,10 +62,10 @@ int	char_check(t_data_load *load)
 		j = 0;
 		while (load->map_matrix[i][j])
 		{
-			if ((load->map_matrix[i][j] != '1') && (load->map_matrix[i][j] != '0') && (load->map_matrix[i][j] != 'P')
-				&& (load->map_matrix[i][j] != 'C') && (load->map_matrix[i][j] != 'E') && (load->map_matrix[i][j] != '\n'))
+			if (characters(load->map_matrix[i][j]))
 			{
-				ft_printf("Invalid item in map: '%c'\n", load->map_matrix[i][j]);
+				ft_printf("Invalid item in map: '%c'\n",
+					load->map_matrix[i][j]);
 				return (1);
 			}
 			j++;
--- src/collision.c
+++ src/collision.c
@@ -6,19 +6,48 @@
 /*   By: ulfernan <ulfernan@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 14:20:56 by ulfernan          #+#    #+#             */
-/*   Updated: 2024/12/15 21:59:40 by ulfernan         ###   ########.fr       */
+/*   Updated: 2024/12/17 14:50:37 by ulfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "longlib.h"
 
+int	exit_check(t_data_load *load, int map_y, int map_x, int open_exit)
+{
+	if (load->map_matrix[map_y][map_x] == 'E')
+	{
+		if (open_exit == 1)
+		{
+			free_matrix(load);
+			free_load(load);
+			exit(0);
+		}
+		return (1);
+	}
+}
+
+int	collection(t_data_load *load, int map_y, int map_x)
+{
+	static int	visited_y = 0;
+	static int	visited_x = 0;
+	static int	collected = 0;
+
+	if (load->map_matrix[map_y][map_x] == 'C' && map_y != visited_y && map_x != visited_x)
+	{
+		collected++;
+		visited_y = map_y;
+		visited_x = map_x;
+		ft_printf("count: %i\n", collected);
+		if (collected == load->collectible_count)
+			return (1);
+	}
+	return (0);
+}
+
 int		collision(t_data_load *load, int x, int y)
 {
 	int			map_x;
     int			map_y;
-	static int	visited_y = 0;
-	static int	visited_x = 0;
-	static int	collected = 0;
 	static int	open_exit = 0;
     
 	map_x = x / load->pixel_x;
@@ -29,26 +58,9 @@ int		collision(t_data_load *load, int x, int y)
 		free_matrix(load);
 		return (1);
 	}
-	if (load->map_matrix[map_y][map_x] == 'C' && map_y != visited_y && map_x != visited_x)
-	{
-		collected++;
-		visited_y = map_y;
-		visited_x = map_x;
-		ft_printf("count: %i\n", collected);
-		if (collected == load->collectible_count)
-			open_exit = 1;
-	}
-	if (load->map_matrix[map_y][map_x] == 'E')
-	{
-		if (open_exit == 1)
-		{
-			free_matrix(load);
-			free_load(load);
-			exit(0);
-		}
+	if (collection(load, map_y, map_x))
+		open_exit = 1;
+	if (exit_check(load, map_y, map_x, open_exit))
 		free_matrix(load);
-		return (1);
-	}
-	free_matrix(load);
 	return (0);
 }
